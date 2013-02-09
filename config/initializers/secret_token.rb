fab5d4972180203a6633561d0f282fc9b1d21a49
Stephen Peek <keepevets@gmail.com>
2013-02-09 18:53:53 -0500

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hearst::Application.config.secret_token = '15538f62776aeccfc6d8335516d11f94b69ba08e6ad321f840e74d1324a7d8de1932eea66e0f67201553d7c3f0052d877cb94cae0195bf06a0df91c7d3c835e9'
